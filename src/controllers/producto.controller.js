facf3b28f7caee69972b93e77dd5eae860ec1b28
cristian <cchura.cpc@gmail.com>
2025-03-18 21:21:15 -0400

    productos

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -10,7 +10,7 @@ export default {
             const page = parseInt(req.query.page || 1);
             const limit = parseInt(req.query.limit || 10);
 
-            const offset = (page - 1) * 10;
+            const offset = (page - 1) * limit;
 
             const productos = await models.Producto.findAndCountAll({
                 where: {
